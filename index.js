75ee5c418cfa4791ddc7e83b8c77387c149d9c16
Thaís Correia <90629181+ThataCorreia@users.noreply.github.com>
2024-02-21 14:38:41 +0000

    modified:   index.js

--- index.js
+++ index.js
@@ -17,4 +17,15 @@ app.get('/item', function (req, res) {
   res.send(lista)
 })
 
+// Read By ID -> [GET] /item/:id
+app.get('/item/:id', function (req, res){
+  const id = req.params.id
+
+  // Acesso item na lista baseado no ID recebido
+  const item = lista[id]
+
+  // Envio o item obtido como resposta http
+  res.send(item)
+} )
+
 app.listen(3000)
